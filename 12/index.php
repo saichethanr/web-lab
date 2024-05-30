837d1a59506429af299240bc7ea0de65becb45d2
saichethanr <saichethan9019@gmail.com>
2024-05-30 08:53:14 +0530

    11 and 12 added

---
+++ 12/index.php
@@ -0,0 +1,17 @@
+<?php
+   if(_SERVER["REQUEST_METHOD"]=="POST"){
+      $username = _POST["name"];
+      $password  = _POST["password"];
+
+      $file = fopen("login.txt","r");
+
+      $content = fgets($file);
+      if($content == $username . ":" . $password){
+        echo "alert('Access granted!')";    
+      }
+      else{
+        echo "alert('Incorrect')";
+      }
+
+   }
+?>
